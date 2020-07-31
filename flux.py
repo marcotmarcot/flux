4bd95ff6c87e399b9d30c8dbdd88ca9d4d51ae80
Marco Gontijo <marcotmarcot@gmail.com>
2020-07-31 20:30:24 -0300

    simplify

--- flux.py
+++ flux.py
@@ -27,21 +27,6 @@ def check_splits_sanity(splits):
         raise Exception(message)
 
 
-def start(flux, name, i, periods):
-    si = str(i)
-    par = "(D" + si + ":" + column(periods) + si + ")"
-    flux.write(name + ",=AVERAGE" + par + ",=SPARKLINE" + par)
-
-
-def column(j):
-    c = ''
-    j += 4
-    while j > 0:
-        c = chr(ord('A') - 1 + + j % 26) + c
-        j /= 26
-    return c
-
-
 class PeriodStr(object):
     def __init__(self, date):
         self.period_str = date.strftime("%Y-%m")
@@ -104,8 +89,8 @@ class Account(object):
         self.add_key(period)
         self.periods[period] += value
 
-    def print_line(self, flux, periods, i):
-        start(flux, self.name, i, len(periods))
+    def print_line(self, flux, periods):
+        flux.write(self.name)
         for period in sorted(periods):
             if period in self.periods:
                 value = self.periods[period]
@@ -121,7 +106,7 @@ class Periods(object):
         self.current_period = PeriodStr(datetime.date.today())
 
     def add_key(self, period):
-        if period == self.current_period:
+        if period >= self.current_period:
             return False
         logging.debug(
             "Periods.add_key: period (" + str(period) +
@@ -130,78 +115,38 @@ class Periods(object):
         return True
 
     def print_periods(self, flux):
-        flux.write(",Average,Line")
         for period in sorted(self.periods):
             flux.write("," + str(period))
         flux.write("\n")
 
-    def print_header(self, flux, pos, neg):
+    def print_header(self, flux):
         self.print_periods(flux)
-        self.print_sum(flux, "Total", 2, 0, pos + neg)
-        self.print_sum(flux, "Positive", 3, 0, pos)
-        self.print_sum(flux, "Negative", 4, pos, pos + neg)
-
-    def print_sum(self, flux, name, i, begin, end):
-        start(flux, name, i, len(self.periods))
-        for j in range(len(self.periods)):
-            c = column(j)
-            flux.write(
-                ",=SUM(" + c + str(5 + begin) + ":" + c + str(5 + end - 1) + ")")
-        flux.write("\n")
-
-    def print_percent(self, flux, name, num, den, i):
-        start(flux, name, i, len(self.periods))
-        for j in range(len(self.periods)):
-            c = column(j)
-            flux.write(",=" + c + str(num) + " / " + c + str(den))
-        flux.write("\n")
 
 
 class Table(object):
     def __init__(self, flux_path):
         self.flux_path = flux_path
         self.periods = Periods()
-        self.pos = {}
-        self.neg = {}
+        self.account = {}
 
-    def add_pos(self, account):
-        logging.debug("Table.add_pos(" + account + ")")
-        if account not in self.pos:
-            self.pos[account] = Account(account)
-
-    def add_neg(self, account):
-        logging.debug("Table.add_neg(" + account + ")")
-        if account not in self.neg:
-            self.neg[account] = Account(account)
+    def add_account(self, account):
+        logging.debug("Table.add_account(" + account + ")")
+        if account not in self.account:
+            self.account[account] = Account(account)
 
     def read_transaction(self, account, period, value):
         if not self.periods.add_key(period):
             return
-        if value > 0:
-            self.add_pos(account)
-            self.pos[account].read_transaction(period, value)
-        if value < 0:
-            self.add_neg(account)
-            self.neg[account].read_transaction(period, value)
+        self.add_account(account)
+        self.account[account].read_transaction(period, value)
 
     def print_accounts(self, flux):
-        i = 5
-        for account in sorted(self.pos):
-            self.pos[account].print_line(flux, self.periods.periods, i)
-            i += 1
-        for account in sorted(self.neg):
-            self.neg[account].print_line(flux, self.periods.periods, i)
-            i += 1
-        for account in sorted(self.pos):
-            self.periods.print_percent(flux, account, i - len(self.pos) - len(self.neg), 3, i)
-            i += 1
-        for account in sorted(self.neg):
-            self.periods.print_percent(flux, account, i - len(self.pos) - len(self.neg), 4, i)
-            i += 1
+        for account in sorted(self.account):
+            self.account[account].print_line(flux, self.periods.periods)
 
     def print_table(self):
         with open(self.flux_path, "w") as flux:
-            self.periods.print_header(flux, len(self.pos), len(self.neg))
+            self.periods.print_header(flux)
             self.print_accounts(flux)
 
 
@@ -236,12 +181,12 @@ class Application(object):
         account, i = self.get_other_account(splits)
         if account is None:
             return
-        value = float(splits[i].value) / 12.0
+        value = float(splits[i].value)
         currency = transaction.currency.name
         if currency == "USD":
-            value *= 3.1671
+            value *= 5.22
         elif currency == "EUR":
-            value *= 3.9195
+            value *= 6.15
         period = PeriodStr(transaction.date)
         self.table.read_transaction(account, period, value)
 
